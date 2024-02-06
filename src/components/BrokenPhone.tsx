8cc4d0edf7de49f9c099361da4f57e06c4213e39
manufores <flaixmanx@hotmail.com>
2024-02-06 12:06:29 +0100

    first BrokenPhone commit

---
+++ src/components/BrokenPhone.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const BrokenPhone = () => {
+  return (
+    <View>
+      <Text>Broken Phone Game</Text>
+    </View>
+  );
+};
